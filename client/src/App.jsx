aa4b1e0fa6859b8b9b34fdfcb5bccfabccf10dc6
Zahid Ul Islam <zahidtwt@gmail.com>
2023-06-10 16:33:52 +0600

    Sign Up Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import QuickLink from './pages/QuickLink';
 import UserLogin from './pages/UserLogin';
+import UserSignup from './pages/UserSignup';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route exact path='/' element={<QuickLink />} />
         <Route exact path='/login' element={<UserLogin />} />
+        <Route exact path='/signup' element={<UserSignup />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ client/src/pages/UserSignup.jsx
@@ -0,0 +1,208 @@
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  HStack,
+  Image,
+  Input,
+  IconButton,
+  InputGroup,
+  InputRightElement,
+  Text,
+  VStack,
+  useToast,
+} from '@chakra-ui/react';
+import React, { useState } from 'react';
+import signupSvg from '../assets/signup.svg';
+import { FcGoogle } from 'react-icons/fc';
+import { HiEye, HiEyeOff } from 'react-icons/hi';
+import MetaIcon from '../components/customIcons/MetaIcon';
+
+const UserSignup = () => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+
+  const toast = useToast();
+
+  const handleFirstNameChange = (e) => {
+    setFirstName(e.target.value);
+  };
+
+  const handleLastNameChange = (e) => {
+    setLastName(e.target.value);
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleConfirmPasswordChange = (e) => {
+    setConfirmPassword(e.target.value);
+  };
+
+  const handleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleShowConfirmPassword = () => {
+    setShowConfirmPassword(!showConfirmPassword);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (password !== confirmPassword) {
+      setPasswordError(true);
+      toast({
+        title: "Passwords doesn't match",
+        status: 'error',
+        duration: 3000,
+        isClosable: true,
+      });
+      return;
+    }
+
+    setPasswordError(false);
+
+    console.log('First Name:', firstName);
+    console.log('Last Name:', lastName);
+    console.log('Email:', email);
+    console.log('Password:', password);
+    console.log('Confirm Password:', confirmPassword);
+  };
+
+  return (
+    <HStack h={'100vh'}>
+      <VStack
+        h={'full'}
+        p={10}
+        spacing={10}
+        justify='center'
+        bgColor={'blue.50'}
+        w={'50vw'}>
+        <Image src={signupSvg} alt='login' width={'40vw'} />
+      </VStack>
+
+      <VStack>
+        <Box w={'50vw'} p={10} align='center'>
+          <Text fontSize='3xl' fontWeight='bold' mb={5}>
+            Registration
+          </Text>
+          <form onSubmit={handleSubmit}>
+            <VStack w={'25vw'} spacing={5}>
+              <HStack w='full'>
+                <FormControl id='firstName' isRequired>
+                  <FormLabel>First Name</FormLabel>
+                  <Input
+                    type='text'
+                    value={firstName}
+                    onChange={handleFirstNameChange}
+                    variant='filled'
+                  />
+                </FormControl>
+                <FormControl id='lastName' isRequired>
+                  <FormLabel>Last Name</FormLabel>
+                  <Input
+                    type='text'
+                    value={lastName}
+                    onChange={handleLastNameChange}
+                    variant='filled'
+                  />
+                </FormControl>
+              </HStack>
+              <FormControl id='email' isRequired>
+                <FormLabel>Email</FormLabel>
+                <Input
+                  type='email'
+                  value={email}
+                  onChange={handleEmailChange}
+                  variant='filled'
+                />
+              </FormControl>
+              <FormControl id='password' isRequired>
+                <FormLabel>Password</FormLabel>
+                <InputGroup>
+                  <Input
+                    type={showPassword ? 'text' : 'password'}
+                    value={password}
+                    onChange={handlePasswordChange}
+                    variant={passwordError ? 'outline' : 'filled'} // Add red outline when passwords don't match
+                    borderColor={passwordError ? 'red.500' : undefined}
+                  />
+                  <InputRightElement>
+                    <IconButton
+                      aria-label={
+                        showPassword ? 'Hide Password' : 'Show Password'
+                      }
+                      icon={showPassword ? <HiEyeOff /> : <HiEye />}
+                      onClick={handleShowPassword}
+                      variant='ghost'
+                    />
+                  </InputRightElement>
+                </InputGroup>
+              </FormControl>
+              <FormControl id='confirmPassword' isRequired>
+                <FormLabel>Confirm Password</FormLabel>
+                <InputGroup>
+                  <Input
+                    type={showConfirmPassword ? 'text' : 'password'}
+                    value={confirmPassword}
+                    onChange={handleConfirmPasswordChange}
+                    variant={passwordError ? 'outline' : 'filled'} // Add red outline when passwords don't match
+                    borderColor={passwordError ? 'red.500' : undefined}
+                  />
+                  <InputRightElement>
+                    <IconButton
+                      aria-label={
+                        showConfirmPassword ? 'Hide Password' : 'Show Password'
+                      }
+                      icon={showConfirmPassword ? <HiEyeOff /> : <HiEye />}
+                      onClick={handleShowConfirmPassword}
+                      variant='ghost'
+                    />
+                  </InputRightElement>
+                </InputGroup>
+              </FormControl>
+              {/* {passwordError && (
+                <Text color='red.500' textAlign={'left'}>
+                  Password doesn't match.
+                </Text>
+              )} */}
+              <Button type='submit' colorScheme='blue' width={'full'}>
+                Sign Up
+              </Button>
+              <Button
+                leftIcon={<FcGoogle size={20} />}
+                type='submit'
+                colorScheme='gray'
+                width={'full'}>
+                Sign up with Google
+              </Button>
+              <Button
+                leftIcon={<MetaIcon size={22} />}
+                type='submit'
+                colorScheme='gray'
+                width={'full'}>
+                Sign up with Meta
+              </Button>
+            </VStack>
+          </form>
+        </Box>
+      </VStack>
+    </HStack>
+  );
+};
+
+export default UserSignup;
