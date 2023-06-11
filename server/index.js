46fa516f45f109bc4f1b92bc6d2307b4e3953e1b
mubtasim <mubtasim91@gmail.com>
2023-06-11 14:28:40 +0600

    removed federatedRouter from index.js

--- server/index.js
+++ server/index.js
@@ -2,12 +2,10 @@ const express = require('express');
 const { connectWithDatabase } = require('./db');
 const { PORT } = require('./config');
 const userRouter = require('./routers/user.route');
-const federatedRouter = require('./routers/googleAuth.route');
 
 const app = express();
 
 // Middlewares
-app.use(express.urlencoded({ extended: false }));
 app.use(
   express.json({
     limit: '10mb',
@@ -20,7 +18,6 @@ app.get('/', (req, res) => {
 });
 
 app.use('/user', userRouter);
-app.use('/thridpartylogin', federatedRouter);
 
 const startServer = async () => {
   try {
