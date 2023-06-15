264c26999100b801bed75a7da102a519a327bcdc
Zahid Ul Islam <zahidtwt@gmail.com>
2023-06-15 12:45:07 +0600

    refresh

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,23 +1,20 @@
-import React from 'react'
-import Navbar from '../components/navbar/navbar'
-import NavbarII from '../components/navbar/NavbarII'
-import NavbarIII from '../components/navbar/NavbarIII'
-import Footer from '../components/footer/footer'
-import PartnerSection from '../components/partnerSection/PartnerSection'
-import PartnerSectionII from '../components/partnerSection/PartnerSectionII'
-import Hero from '../components/heroSection/hero'
-import HowItWorks from '../components/howItWorks/howItWorks'
+import React from 'react';
+import NavbarIII from '../components/navbar/NavbarIII';
+import Footer from '../components/footer/footer';
+import PartnerSection from '../components/partnerSection/PartnerSection';
+import Hero from '../components/heroSection/hero';
+import HowItWorks from '../components/howItWorks/howItWorks';
 
 const LandingPage = () => {
   return (
     <div>
-        <NavbarIII></NavbarIII>
-        <Hero></Hero>
-        <HowItWorks></HowItWorks>
-        <PartnerSection></PartnerSection>
-        <Footer></Footer>
+      <NavbarIII></NavbarIII>
+      <Hero></Hero>
+      <HowItWorks></HowItWorks>
+      <PartnerSection></PartnerSection>
+      <Footer></Footer>
     </div>
-  )
-}
+  );
+};
 
-export default LandingPage
+export default LandingPage;
